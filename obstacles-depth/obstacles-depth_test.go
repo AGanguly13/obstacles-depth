834869b8312cf9dadb624485205606d278e663a6
AGanguly13 <adwaitganguly@viam.com>
2025-06-23 16:21:02 -0400

    initial test commit

---
+++ obstacles-depth/obstacles-depth_test.go
@@ -0,0 +1,34 @@
+package obstaclesdepth
+
+import (
+	"Image"
+	"os"
+	"testing"
+
+	"go.viam.com/rdk/components/camera"
+
+	"go.viam.com/test"
+
+)
+
+func TestGetObjectPointCloud(t *testing.T) {
+
+}
+
+func TestGetProperties(t *testing.T) {
+
+}
+
+func TestCaptureAllFromCamera(t *testing.T) {
+
+}
+
+func TestValidate() {
+	// case 1: empty config
+	conf = &obsDepthConfig{}
+	// case 2: expected config
+	conf_empty = &obsDepthConfig{
+	}
+}
+
+
